a5dd7ea6398c89560c50f4cfc396cf178c381f91
karthik-n-hebbar <karthiknh125@gmail.com>
2023-06-24 03:09:07 +0530

    app dashboard

--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -13,16 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        exitButton = findViewById(R.id.button6);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Exit the app
-                finish();
-                System.exit(0);
-            }
-        });
     }
 
 
