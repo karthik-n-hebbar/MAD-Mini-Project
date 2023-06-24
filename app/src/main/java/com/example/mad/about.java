3d6179f0338ac7e0a1e45df3fb6e99f1cec10009
karthik-n-hebbar <karthiknh125@gmail.com>
2023-06-25 00:06:15 +0530

    home layout

---
+++ app/src/main/java/com/example/mad/about.java
@@ -0,0 +1,12 @@
+package com.example.mad;
+
+import android.os.Bundle;
+
+public class aboutus extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus);
+    }
+}
---
+++ app/src/main/java/com/example/mad/garage.java
@@ -0,0 +1,2 @@
+package com.example.mad;public class garage {
+}
